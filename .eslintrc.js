e1869d4eef8333ee14033d2471dd54360215bb05
zy694567612@gmail.com <zy694567612@gmail.com>
2021-08-17 01:12:47 +0800

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:prettier/recommended',
+    'airbnb-base',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'import/extensions': [
+      2,
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        json: 'never',
+        js: 'never',
+      },
+    ],
+    // storybook
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+  },
+};
