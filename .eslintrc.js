ac9df826c57f8427cb8f15ab8530aab425401680
zy694567612@gmail.com <zy694567612@gmail.com>
2021-08-18 01:09:51 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,5 +28,12 @@ module.exports = {
     ],
     // storybook
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'linebreak-style': 0,
   },
 };
